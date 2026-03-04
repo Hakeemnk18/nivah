9de77cded2ee6c3dcfd222e069f59906aa5dd3fb
Hakeemnk18 <hakeemnk18@gmail.com>
2026-03-04 22:48:24 +0530

    add vercel url in cors

--- backend/src/core/app.ts
+++ backend/src/core/app.ts
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cookieParser())
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:4173"],
+  origin: ["http://localhost:5173", "http://localhost:4173", "https://nivah-rho.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
