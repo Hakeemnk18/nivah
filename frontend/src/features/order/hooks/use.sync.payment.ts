1a21a7c28ee2a4b475c991674c2b0fa08de294f8
Hakeemnk18 <hakeemnk18@gmail.com>
2026-03-06 17:48:43 +0530

    fix(webhook) add request type

--- frontend/src/features/order/hooks/use.sync.payment.ts
+++ frontend/src/features/order/hooks/use.sync.payment.ts
@@ -1,4 +1,4 @@
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { syncPaymentApi } from "../api/order.api";
 
 export const useSyncPayment = () => {
