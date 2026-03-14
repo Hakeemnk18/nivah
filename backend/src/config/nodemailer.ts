538616a225a45a2a96f023c75bccd532d831329f
Hakeemnk18 <hakeemnk18@gmail.com>
2026-03-14 10:29:52 +0530

    add ipv4

--- backend/src/config/nodemailer.ts
+++ backend/src/config/nodemailer.ts
@@ -1,11 +1,13 @@
 import nodemailer from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport/index.js";
 
 export const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT) || 587,
-    secure: false, // true for 465, false for other ports
+    secure: false,
+    family: 4,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
     },
-});
+} as SMTPTransport.Options);
